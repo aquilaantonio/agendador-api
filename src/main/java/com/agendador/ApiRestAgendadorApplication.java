b56f295a9858d8c43cea48381e2d7289a300569e
Aquila Pereira <aquila.antonio@hotmail.com>
2020-12-12 15:54:02 -0300

    primeiro commit

---
+++ src/main/java/com/agendador/ApiRestAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAgendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendador/ApiRestAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
